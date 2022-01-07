ad620a954445e36ad47576db4046133b3b457961
ZoranUTF8 <janjiczoran23@gmail.com>
2022-01-07 10:24:49 +0900

    add headers in basic api

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -1,8 +1,10 @@
 const data = require("../assets/data");
 
 exports.handler = async (event, context) => {
-
   return {
+    headers: {
+      "Access-Control-Alow-Origin": "*",
+    },
     statusCode: 200,
     body: JSON.stringify(data),
   };
